568e785ecf4d4ad4a4bcd622f76043463f3a0cd4
putixin <jiang_xiao_bin@163.com>
2017-07-25 01:44:23 +0000

    fix a syntax error of show_follow.blade.php

--- resources/views/users/show_follow.blade.php
+++ resources/views/users/show_follow.blade.php
@@ -8,7 +8,7 @@
     @foreach ($users as $user)
       <li>
         <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="gravatar"/>
-        <a href="{{ rouate('users.show', $user->id) }}" class="username">{{ $user->name }}</a>
+        <a href="{{ route('users.show', $user->id) }}" class="username">{{ $user->name }}</a>
       </li>
     @endforeach
   </ul>
